8d279f45fcfaca5b5890928b524bec73e4f9c049
Unknown Author <eugenekip2016@gmail.com>
2018-12-25 05:56:40 +0300

    Add density between points, create example parametrs, add graphics

--- kepler.py
+++ kepler.py
@@ -6,87 +6,91 @@ import random
 import math
 
 #Little time range
-delta_t = 0.01 
+delta_t = 0.01
 
 #gravitational constant
 G = 50.0
 
-#Colliding points distance 
+#Colliding points distance
 collision_distance = 0.05
 
 #All time
-all_time = 100
+all_time = 50
 
 #Material point on the flat
 class MatPoint:
-	
 	def __init__(self, mass: 'float', position: 'numpy.array', velocity: 'numpy.array'):
 		self.mass = mass
 		self.position = position
 		self.velocity = velocity
-	
+
 	#Interaction between points
 	@staticmethod
 	def dencity(distance: 'float')-> 'float':
 		if (distance > collision_distance):
-			return G / distance ** 1
+			return G / distance ** 2
 		else:
 			#Collision
-			return - G / distance ** 3 
-	
-	#Vector force induced by other point 
+			return - G / distance ** 3
+
+	#Vector force induced by other point
 	def force(self, other: 'MatPoint')-> 'numpy.array':
 		force = numpy.array([0, 0])
 		dr = other.position - self.position
 		distance = numpy.linalg.norm(dr)
+		if (distance == 0):
+			#Dencity with the same point
+			return force
 		force = dr * other.mass * self.mass * self.dencity(distance) / distance
 		return force
-		
+
 	def apply_force(self, force: 'numpy.array'):
-		self.velocity[0] += force[0] / self.mass * delta_t
-		self.velocity[1] += force[1] / self.mass * delta_t
-	
+		self.velocity += force / self.mass * delta_t
+
 	#Move the point on delta_t
 	def move(self):
-		self.position[0] += self.velocity[0] * delta_t 
-		self.position[1] += self.velocity[1] * delta_t
+		self.position += self.velocity * delta_t
 
-points_count = 1
+#Count of moveable points
+points_count = 3
+#Moveable points
 points = []
-centrum = MatPoint(10.0, numpy.array([0.0, 0.0]), numpy.array([0.0, 0.0]))
 
 #Define points start positions, velocities and masses
 def init():
+	Masses = [2.0, 10.0, 1.0]
+	X = [0.0, 0.0, 0.0]
+	Y = [15.0, 0.0, -10.0]
+	velocities_X = [5.0, 0.0, -10.0]
+	velocities_Y = [0.0, 0.0, 0.0]
 	for i in range(0, points_count):
-		velocity = random.uniform(-2, 2) + 7.0
-		points.append(MatPoint(1.0, numpy.array([1.0, 10.0]), 
-		numpy.array([velocity, 0.0])))
-		print(velocity)
-		
+		points.append(MatPoint(Masses[i], numpy.array([X[i], Y[i]]), numpy.array([velocities_X[i], velocities_Y[i]])))
+
 init()
 
 positions_X = []
 positions_Y = []
 
+for numpoint in range(0, points_count):
+	positions_X.append([])
+	positions_Y.append([])
+
+#Move
+
 count_of_steps = int(all_time / delta_t)
 
-numpoint = 0
-for point in points:
-	for n in range(0, count_of_steps):
-		positions_X.append(point.position[0])
-		positions_Y.append(point.position[1])
+for n in range(0, count_of_steps):
+	for point in points:
 		point.move()
-		point.apply_force(point.force(centrum))
-	plt.plot(positions_X, positions_Y)
-	numpoint += 1
+		for other in points:
+			point.apply_force(point.force(other))
+	for numpoint in range(0, points_count):
+		positions_X[numpoint].append(points[numpoint].position[0])
+		positions_Y[numpoint].append(points[numpoint].position[1])
+
+#Draw
+
+for numpoint in range(0, points_count):
+	plt.plot(positions_X[numpoint], positions_Y[numpoint])
 
 plt.show()
-
-	
-
-
-
-
-
-	
-	
